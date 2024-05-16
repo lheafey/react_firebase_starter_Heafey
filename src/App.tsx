db37ad17d4cc2156a07c0e0ee1369dff7de31c55
Barry Lawson <blawson@bates.edu>
2024-05-16 07:55:51 -0400

    updated container_div style in App

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <div id="page-div" className="h-lvh grid place-content-center">
       <div
         id="container-div"
-        className="bg-gray-300 text-black w-[600px] h-[550px] border-solid border-black border-2"
+        className="flex fex-col bg-gray-300 text-black w-[600px] h-[550px] border-solid border-black border-2"
       >
         <div id="signup-login-div" className="flex flex-row content-between">
           <Signup />
